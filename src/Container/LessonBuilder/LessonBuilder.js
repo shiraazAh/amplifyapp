0b24d2279d27db78f2d181770b5c2c43da76a9ee
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-08-03 15:34:49 +0530

    Delete and Update set up properly with comments

--- src/Container/LessonBuilder/LessonBuilder.js
+++ src/Container/LessonBuilder/LessonBuilder.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { API, Storage } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from '../../graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation, updateNote as updateNoteMutation } from '../../graphql/mutations';
+import { deleteNote as deleteNoteMutation, updateNote as updateNoteMutation } from '../../graphql/mutations';
+
+import '../../App.css';
+
 // import Editor from './Component/Editor'
 
-const initialFormState = { name: '', description: '', title: '', component: ''}
-const initialNoteState = {id:'', name: '', description: '', title: ''}
+const initialFormState = { name: '', description: '', title: ''};
+let Id = 0;
 
-function App() {
-  const [notes, setNotes] = useState([]);
+function LessonBuilder() {
+  const [notes, setNotes] = useState({id:'', name: '', description: '', title: '', component: []});
   const [formData, setFormData] = useState(initialFormState);
-  const [NoteData, setNoteData] = useState(initialNoteState);
 
   useEffect(() => {
     fetchNotes();
@@ -26,33 +27,20 @@ function App() {
   }
 
   async function fetchNotes() {
+
+    // Get data from database
     const apiData = await API.graphql({ query: listNotes });
     const allData = apiData.data.listNotes.items;
 
-    //removing unwanted characters in createdAt Date
-    for(let obj of allData){
-      obj.createdAt = obj.createdAt.replace(/[-T:.Z]/g, '');
-    }
+    // Storing the data in variable
+    const notesFromAPI = allData[0];
 
-    //Ordering it according to createdAt Date
-    allData.sort((a, b) => {   const objDateA = a.createdAt;
-      const objDateB = b.createdAt;
-  
-      let comparison = 0;
-      if (objDateA > objDateB) {
-        comparison = 1;
-      } else if (objDateA < objDateB) {
-        comparison = -1;
-      }
-      return comparison;});
+    // Parsing the JSON element
+    const parsedComponent = JSON.parse(notesFromAPI.component);
+    allData[0].component = parsedComponent;
 
-    console.log(allData);
-    
-    const notesFromAPI = allData.map(obj => JSON.parse(obj.component));
-
-    console.log(notesFromAPI);
-
-    await Promise.all(notesFromAPI.map(async note => {
+    // If there is an image, get from storage
+    await Promise.all(parsedComponent.map(async note => {
       if (note.image) {
         const image = await Storage.get(note.image);
         note.image = image;
@@ -60,49 +48,99 @@ function App() {
       return note;
     }))
 
+    // Set the state to the value
     setNotes(notesFromAPI);
 
   }
 
-  async function createNote() {
+  // async function createNote() {
+  //   if (!formData.name || !formData.description) return;
+  //   let comp = formData;
+  //   delete comp.component;
+  //   formData.component = JSON.stringify(comp);
+  //   await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+  //   if (formData.image) {
+  //     const image = await Storage.get(formData.image);
+  //     console.log(image);
+  //     formData.image = image;
+  //   }
+  //   setNotes([ ...notes, formData ]);
+  //   setFormData(initialFormState);
+  // }
+
+// Funtion to update the data
+  async function updateNote() {
+    // Only allow to run the function if there is no name or description 
     if (!formData.name || !formData.description) return;
-    let comp = formData;
-    delete comp.component;
-    formData.component = JSON.stringify(comp);
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    if (formData.image) {
-      const image = await Storage.get(formData.image);
-      console.log(image);
-      formData.image = image;
-    }
-    setNotes([ ...notes, formData ]);
+    let newArr = notes;
+    const newForm = formData;
+
+    // Add an new Id to all Elements
+    newForm.id = Id;
+    Id = Id + 1;
+    
+    // Add the new form to the component array and convert to JSON
+    newArr.component.push(newForm);
+    newArr.component = JSON.stringify(newArr.component);
+
+    // Delete createdAt and updatedAt and createdAt as it is unwanted in updating element
+    delete newArr.createdAt;
+    delete newArr.updatedAt;
+
+    // Send the new updated version to the data base
+    await API.graphql({ query: updateNoteMutation, variables: { input: newArr } });
+
+    // Alert to show its done
+    alert("Edited");
+
+    // Parse JSON and save the new element to the state
+    newArr.component = JSON.parse(newArr.component);
+    setNotes(newArr);
+
+    // Set formData to initial state
     setFormData(initialFormState);
   }
 
-  async function updateNote(index) {
-    await API.graphql({ query: updateNoteMutation, variables: { input: NoteData } });
-    alert("Edited")
-    setNoteData(initialNoteState)
+// Function to delete data
+  async function deleteComponent(item) {
+    // Get the current data from the state
+    let newArrAfterDelete = notes;
+
+    // Filter the element and only create an array of needed elements
+    const newComp = newArrAfterDelete.component.filter(i => i.name !== item.name);
+    newArrAfterDelete.component = JSON.stringify(newComp);
+
+    // Delete createdAt and updatedAt and createdAt as it is unwanted in updating element
+    delete newArrAfterDelete.createdAt;
+    delete newArrAfterDelete.updatedAt;
+
+    // Update the database with new updated data
+    await API.graphql({ query: updateNoteMutation, variables: { input: newArrAfterDelete } });
+
+    // Alert to show its done
+    alert("Deleted");
+
+    // Set the new updated component element to the state
+    setNotes({id: newArrAfterDelete.id, name: newArrAfterDelete.name, description: newArrAfterDelete.description, title: newArrAfterDelete.title, component: newComp});
+
   }
 
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
 
-  function switchNoteHandler(event, index){ 
-    const newArr = [...notes];
-    newArr[index].description = event.target.value
-    const newData = { 
-      id: newArr[index].id,
-      name: newArr[index].name,
-      description: newArr[index].description,
-      title: newArr[index].title,
-    }
-    setNoteData(newData)
-    setNotes(newArr);
-  }
+  // async function deleteNote({ id }) {
+  //   const newNotesArray = notes.component.filter(note => note.id !== id);
+  //   setNotes(newNotesArray);
+  //   await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  // }
+
+  // function switchNoteHandler(){ 
+  //   const newArr = notes;
+  //   const newArrComponent = newArr.component;
+  //   const newForm = formData;
+  //   newArrComponent.push(newForm);
+  //   setFormData(initialFormState);
+  //   setNotes(newArr);
+  //   updateNote();
+  // }
 
   return (
     <div className="App">
@@ -130,20 +168,15 @@ function App() {
         value={formData.description}
       />
 
-      <button onClick={createNote}>Create</button>
+      <button onClick={updateNote}>Create</button>
       <div style={{marginBottom: 30}}>
       {
-        notes.map((note, i) => (
+        notes.component.map((note, i) => (
           <div key={note.id || note.name}>
             {note.title ? <h1 className="title">{note.title}</h1> : null}
             <h2 className="subtitle">{note.name}</h2>
             <p className="subtitle">{note.description}</p>
-            <input 
-              type="text" 
-              placeholder={note.description} 
-              onChange={(event) => switchNoteHandler(event, i)} />
-            <button onClick={() => updateNote(i)}>Edit</button>
-            <button onClick={() => deleteNote(note)}>Delete text</button>
+            <button onClick={() => deleteComponent(note)}>Delete text</button>
             {
               note.image && <img src={note.image} style={{width: 400}} alt="img" />
             }
@@ -151,9 +184,8 @@ function App() {
         ))
       }      
       </div>
-      <AmplifySignOut />
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default LessonBuilder;
