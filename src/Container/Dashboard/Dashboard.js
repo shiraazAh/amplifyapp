0f63b3b2f2876be6633c3eb2749498c4d3a9d1b9
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-08-06 20:49:58 +0530

    Lesson plan is recieved from databse and then passed on to the cards if it is finished editing

--- src/Container/Dashboard/Dashboard.js
+++ src/Container/Dashboard/Dashboard.js
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Toolbar from '../../Component/Navigation/Toolbar/Toolbar';
 import Card from '../../Component/UI/Card/Card';
 import Button from '@material-ui/core/Button';
 import NavigationItem from '../../Component/Navigation/NavigationItem/NavigationItem';
 
+import { API, Storage } from 'aws-amplify';
+import { listNotes } from '../../graphql/queries';
+
 import classes from './Dashboard.module.css'
 
-class Dashboard extends Component {
-    render() {
+const Dashboard = (props) => {
+
+    const [data, setData] = useState([]);
+    const [SpinnerHandler, setSpinnerHandler] = useState(true);
+    
+    useEffect(() => {
+        fetchNotes(); 
+    }, []);
+
+    async function fetchNotes() {
+
+    // Get data from database
+    const apiData = await API.graphql({ query: listNotes });
+    const allData = apiData.data.listNotes.items;
+
+    // Storing the data in variable
+    let notesFromAPI = allData;
+
+    // Parsing the JSON element
+    const parsedComponent = notesFromAPI.map(note => { return {...note, 'component': JSON.parse(note.component)}});
+
+    // If there is an image, get from storage
+    // await Promise.all(parsedComponent.map(async note => {
+    //   if (note.image) {
+    //     const image = await Storage.get(note.image);
+    //     note.image = image;
+    //   }
+    //   return note;
+    // }))
+
+    // Set the state to the value
+    setData(parsedComponent);
+    setSpinnerHandler(false);
+  }
         return (
             <div className={classes.Dashboard}>
                 <Toolbar></Toolbar>
                 <p>Choose a Lesson Plan or <span><Button><NavigationItem path='/lessonbuilder' exact>Create Lesson Plan</NavigationItem></Button></span></p>
                 <div className={classes.Cards}>
-                <Card />
-                <Card />
-                <Card />
+                {data ? data.map(el => el.editing ? null : <Card key={el.name} title={el.name} subtitle="whatever"/>) : null }
                 </div>
             </div>
         )
-    }
 }
 
 export default Dashboard;
